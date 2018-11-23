aa5c5da32c6ad8aaa4489fbc773308e954f02ac8
Chawye Hsu <h404bi@gmail.com>
2018-11-23 11:31:17 +0800

    Update command-line-token-color-mapping.ps1

--- scripts/command-line-token-color-mapping.ps1
+++ scripts/command-line-token-color-mapping.ps1
@@ -37,17 +37,18 @@ if (Get-Module -ListAvailable -Name "PSReadline") {
         $options.TypeForegroundColor      = "DarkYellow"  # base0A
         $options.VariableForegroundColor  = "DarkRed"     # base08
     } else {
-        $options = Get-PSReadlineOption
         # Token Foreground                                # base16 colors
-        $options.CommandColor   = "$([char]0x1b)[34m"     # base0D
-        $options.CommentColor   = "$([char]0x1b)[93m"     # base03
-        $options.KeywordColor   = "$([char]0x1b)[35m"     # base0E
-        $options.MemberColor    = "$([char]0x1b)[34m"     # base0D
-        $options.NumberColor    = "$([char]0x1b)[91m"     # base09
-        $options.OperatorColor  = "$([char]0x1b)[36m"     # base0C
-        $options.ParameterColor = "$([char]0x1b)[91m"     # base09
-        $options.StringColor    = "$([char]0x1b)[32m"     # base0B
-        $options.TypeColor      = "$([char]0x1b)[33m"     # base0A
-        $options.VariableColor  = "$([char]0x1b)[31m"     # base08
+        Set-PSReadLineOption -Colors @{
+            "Command"   = [ConsoleColor]::DarkBlue        # base0D
+            "Comment"   = [ConsoleColor]::Yellow          # base03
+            "Keyword"   = [ConsoleColor]::DarkMagenta     # base0E
+            "Member"    = [ConsoleColor]::DarkBlue        # base0D
+            "Number"    = [ConsoleColor]::Red             # base09
+            "Operator"  = [ConsoleColor]::DarkCyan        # base0C
+            "Parameter" = [ConsoleColor]::Red             # base09
+            "String"    = [ConsoleColor]::DarkGreen       # base0B
+            "Type"      = [ConsoleColor]::DarkYellow      # base0A
+            "Variable"  = [ConsoleColor]::DarkRed         # base08
+        }
     }
 }
